631433d66dd33b19d26fc18fab840d9d64886e91
Damon Myers <damon.shane.myers@gmail.com>
2016-04-26 15:09:39 -0500

    Added favicon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 
 var app = express();
 
 app.use(express.static('static'));
+app.use(favicon(path.join(__dirname, 'static', 'images', 'favicon.ico')));
 
 app.get('/', function(req, res) {
 	res.render('index.html');
